8af11da64f8201a88f805439f103ee6552294ab1
ceotocende <andreyserov03@gmail.com>
2026-03-11 22:49:44 +0300

    edit file

--- src/events/Events/AddMemberGuild.ts
+++ src/events/Events/AddMemberGuild.ts
@@ -24,10 +24,31 @@ client.on('guildMemberAdd', async member => {
         .setColor(Colors.Green)
         .setTimestamp()
 
+
+        try {
+            const editNickname = await member.setNickname('/inv ("Ник в игре")');
+            
+            embed.setFields({
+                name: 'Никнейм изменен',
+                value: `С \`${member.displayName}\` на \`/inv ("Ник в игре")\``
+            })
+        } catch (err) {
+            console.error(err);
+            embed.setFields({
+                name: 'Произошла ошибка в изменении никнейма',
+                value: `С \`${member.displayName}\` на \`/inv ("Ник в игре")\`\n${err}`
+            })
+        }
+
+
     if (member.avatarURL()) {
         embed.setThumbnail(member.avatarURL())
     }
 
-    if (!channel) return;
-    else await channel.send({ embeds: [ embed ] })
+
+    if (!channel) {
+        return;
+    } else {
+        await channel.send({ embeds: [embed] });
+    };
 })
